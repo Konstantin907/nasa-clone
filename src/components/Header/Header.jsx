ebd7f9e4cd1350ca663e2a6ebbb79d2fd53758b6
konstantin <konstantinivanovic12345@gmail.com>
2024-05-06 19:56:42 -0700

    adding modal to the workspace

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./header.css"
 import { FaAngleDown } from "react-icons/fa";
+import ExploreModal from '../Modal/ExploreModal';
 
 
 const Header = () => {
+    const [modal, setModal] = useState(false);
+
+
   return (
     <div className='header-container'>
         <div className="left-part">
-                <h4 className="explore-heading">Explore <FaAngleDown style={{cursor:"pointer"}}/></h4>   
+                <h4 className="explore-heading" onClick={()=>setModal(!modal)}>
+                    Explore <FaAngleDown style={{cursor:"pointer"}}/>
+                </h4>   
+
+                {/* modal */}
+                {modal && <ExploreModal />}
+           
             <input 
                 type="text" 
                 placeholder='Search...'
                 className='input-search'
             />
+            
         </div>
         <div className="middle-part">
             <img src="/nasa-logo.jpg" alt="logo" className="logo-nasa" />
@@ -25,6 +36,7 @@ const Header = () => {
                 <li>NASA +</li>
             </ul>
         </div>
+        
     </div>
   )
 }
---
+++ src/components/Modal/ExploreModal.jsx
@@ -0,0 +1,116 @@
+import React from 'react'
+import "./modal.css"
+
+const ExploreModal = () => {
+  return (
+    <div className='explore-modal'>
+        <div className="left-explore">
+            <ul className="explore-list">
+                <li className="explore-link">
+                    Home
+                </li>
+                <li className="explore-link">
+                    Missions
+                </li>
+                <li className="explore-link">
+                    Humans in Space
+                </li>
+                <li className="explore-link">
+                    Earth & Climate
+                </li>
+                <li className="explore-link">
+                    The Solar System
+                </li>
+                <li className="explore-link">
+                    The Universe
+                </li>
+                <li className="explore-link">
+                    Science
+                </li>
+                <li className="explore-link">
+                    Aeronautics
+                </li>
+                <li className="explore-link">
+                    Technology
+                </li>
+                <li className="explore-link">
+                    Learning Resources
+                </li>
+
+                <li className="explore-link">
+                    About NASA
+                </li>
+            </ul>
+            
+        </div>
+        <div className = "vertical"></div>
+
+        {/* Right part */}
+            <div className="exploreRightPart">
+                <h5 className="featuredHeading">
+                    FEATURED
+                </h5>
+                <div className="exploreCard">
+                    <div className="singleCard">
+                         <img 
+                            src="/helpinganimal.jpg" 
+                            alt="" 
+                            className='exploreImg'
+                        /> 
+                        <span className="minuteRead">4 min read</span>
+                        <h3 className="articleExploreHeading">
+                            NASA is helping protecting animals around the world
+                            all species of cats and birds.
+                        </h3>
+                        <div className="smallArticleInfo">
+                            <span className="all">ARTICLE</span><span className='sm-text'>1 WEEK AGO</span>
+                        </div>
+                    </div>
+                {/* 2nd card */}  
+
+                <div className="singleCard">
+                         <img 
+                            src="/spaceearth.webp" 
+                            alt="" 
+                            className='exploreImg'
+                        /> 
+                        <span className="minuteRead">6 min read</span>
+                        <h3 className="articleExploreHeading">
+                            Everything you need to know about our blue planet,
+                            all the facts about our planet. 
+                        </h3>
+                        <div className="smallArticleInfo">
+                            <span className="all">ARTICLE</span><span className='sm-text'>3 WEEK AGO</span>
+                        </div>
+                    </div>
+
+                    {/* 3 */}
+                    <div className="singleCard">
+                         <img 
+                            src="/nasarockets.webp" 
+                            alt="" 
+                            className='exploreImg'
+                        /> 
+                        <span className="minuteRead">8 min read</span>
+                        <h3 className="articleExploreHeading">
+                            Our brief history of developing rockets for all the space research and 
+                            and life in space. 
+                        </h3>
+                        <div className="smallArticleInfo">
+                            <span className="all">ARTICLE</span><span className='sm-text'>1 WEEK AGO</span>
+                        </div>
+                    </div>
+
+
+                </div>
+                
+                
+
+
+
+            </div>
+    </div>
+  )
+}
+
+export default ExploreModal
