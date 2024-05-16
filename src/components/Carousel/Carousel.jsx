625f7f014e062216f2dcdd153d7e1757c4802260
Konstantin <konstantinivanovic12345@gmail.com>
2024-05-15 20:47:34 -0700

    fixing css 2

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react'
-import "./carousel.css"
+import "./carousel.css" 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
--- src/components/Modal/NewsModal/NewsModal.jsx
+++ src/components/Modal/NewsModal/NewsModal.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./newsModal.css"
 import { FaArrowRight } from "react-icons/fa6";
 
-
+ 
 const NewsModal = () => {
   return (
     <div className='newsModal'>
