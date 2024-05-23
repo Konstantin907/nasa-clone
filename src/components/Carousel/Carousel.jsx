e1028e031697ec2c7bdae50e554c8cbd4e8104cc
Konstantin <konstantinivanovic12345@gmail.com>
2024-05-23 12:32:40 -0700

    fixing css 7

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -4,7 +4,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { FaArrowUpLong } from "react-icons/fa6";
-
+ 
 
 const Carousel = () => {
     const sliderRef = useRef(null)
--- src/components/FeatureNews/Featured.jsx
+++ src/components/FeatureNews/Featured.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./featured.css"
+import "./featured.css" 
 import { FaSquareArrowUpRight } from "react-icons/fa6";
 
 const Featured = () => {
